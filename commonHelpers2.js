15cd9a78be80e869fd5a40cf1a2079e7e903ddac
GolovskoyNickolay <golovskoy.nickolay@gmail.com>
2025-03-09 21:49:53 +0000

    Deploying to gh-pages from @ GolovskoyNickolay/goit-advancedjs-hw-02@889ad8a53d2675ed16c929b62d42d8206d623748 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{i}from"./assets/vendor-69b9e915.js";const s=document.querySelector(".form");s.addEventListener("submit",o=>{o.preventDefault();const t=Number(s.elements.delay.value),r=s.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{r==="fulfilled"?e(t):m(t)},t)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
